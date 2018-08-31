12a4bbe55a518a755c8bc209e8bd14cad8cf95d0
Nathan Carroll <ntc238@gmail.com>
2018-08-31 14:57:52 -0500

    database setup commands

--- database.sql
+++ database.sql
@@ -1,5 +1,87 @@
-CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
+-- These SQL commands should be run in order to initialize the tables needed for the project.
+
+CREATE TABLE "national_trainer" (
+  "national_trainer_id" SERIAL PRIMARY KEY,
+  "first_name" varchar(255) NOT NULL,
+  "last_name" varchar(255) NOT NULL,
+  "user_name" varchar(255) UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "email" varchar(255) NOT NULL,
+  "title" varchar(255),
+  "status" boolean DEFAULT true
+);
+
+CREATE TABLE "state_lead" (
+  "state_lead_id" SERIAL PRIMARY KEY,
+  "first_name" varchar(255) NOT NULL,
+  "last_name" varchar(255) NOT NULL,
+  "email" varchar(255),
+  "phone_number" varchar(255),
+  "title" varchar(255),
+  "state_level_organization" varchar(255),
+  "state" varchar(255) NOT NULL
+);
+
+CREATE TABLE "requirements" (
+  "requirements_id" SERIAL PRIMARY KEY,
+  "name" varchar(255) UNIQUE NOT NULL,
+  "description" text,
+  "duration" integer NOT NULL,
+  "notification_1_time" integer,
+  "notfication_2_time" integer
+);
+
+CREATE TABLE "cohort" (
+  "cohort_id" SERIAL PRIMARY KEY,
+  "name" varchar(255) NOT NULL,
+  "state" varchar(255) NOT NULL,
+  "start_date" date NOT NULL,
+  "description" text,
+  "state_lead_ref_id" INTEGER REFERENCES state_lead(state_lead_id)
+);
+
+CREATE TABLE "cohort_requirements" (
+  "cohort_req_id" SERIAL PRIMARY KEY,
+  "cohort_id" INTEGER REFERENCES cohort(cohort_id),
+  "requirement_id" INTEGER REFERENCES requirements(requirements_id),
+  "due_date" date NOT NULL,
+  "notes" text,
+  "notification_1_date" date,
+  "notification_2_date" date
+);
+
+CREATE TABLE "local_trainers" (
+  "local_trainers_id" SERIAL PRIMARY KEY,
+  "first_name" varchar(255) NOT NULL,
+  "last_name" varchar(255) NOT NULL,
+  "title" varchar(255),
+  "email" varchar(255),
+  "phone_number" varchar(255),
+  "organization" varchar(255),
+  "district" varchar(255),
+  "state" varchar(255),
+  "state_level_organization" varchar(255),
+  "cohort_ref_id" INTEGER REFERENCES cohort(cohort_id),
+  "status" boolean DEFAULT true,
+  "notes" text
+);
+
+CREATE TABLE "local_trainers_requirements" (
+  "local_trainers_requirements_id" SERIAL PRIMARY KEY,
+  "local_trainers_ref_id" INTEGER REFERENCES local_trainers(local_trainers_id),
+  "requirements_ref_id" INTEGER REFERENCES requirements(requirements_id),
+  "national_trainer_ref_id" INTEGER REFERENCES national_trainer(national_trainer_id),
+  "completed" boolean DEFAULT false,
+  "notes" text,
+  "scheduled_date" date,
+  "notification_1_sent" boolean DEFAULT false,
+  "notification_2_sent" boolean DEFAULT false
+);
+
+CREATE TABLE "national_trainer_notification" (
+  "national_trainer_notification_id" SERIAL PRIMARY KEY,
+  "national_trainer_id" INTEGER REFERENCES national_trainer(national_trainer_id),
+  "local_trainers_requirements" INTEGER REFERENCES local_trainers_requirements(local_trainers_requirements_id),
+  "notification_1_sent" boolean DEFAULT false,
+  "notification_2_sent" boolean DEFAULT false
 );
