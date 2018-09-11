852b2fd52e645ce7375bb13135caab582b9df614
Nathan Carroll <ntc238@gmail.com>
2018-09-11 17:35:32 -0500

    select/unselect all on main trainer search page

--- src/components/TrainerSearchSidebar/TrainerSearchSidebar.js
+++ src/components/TrainerSearchSidebar/TrainerSearchSidebar.js
@@ -32,55 +32,91 @@ class TrainerSearchSidebar extends Component {
     let stateLevelOrgCheckboxes = null;
     let cohortNameCheckboxes = null;
 
-    if (this.props.state_name !== undefined){
-        const stateArray = Array.from(this.props.state_name);
+    if (this.props.checkboxesDisplayed.state_name !== null && this.props.checkboxesSelected.state_name){
+        const stateArray = Array.from(this.props.checkboxesDisplayed.state_name);
         stateCheckboxes = stateArray.map((setElement) => {
             return(
                 <div>
                     <Checkbox
                         value={setElement}
                         name="state"
-                        defaultChecked
+                        checked={this.props.checkboxesSelected.state_name.has(setElement)}
                         onChange={this.props.handleStateCheckbox}
                     />
                     <span>{setElement}</span>
                 </div>
             )
         })
+        if (stateCheckboxes.length > 0){
+            stateCheckboxes = [<div>
+                                <Checkbox
+                                    defaultChecked
+                                    onChange={this.props.handleStateCheckbox}
+                                    value="all"
+                                />
+                                <span>Select/Unselect All</span>
+                                </div>, 
+                                ...stateCheckboxes
+                            ]
+        }
     }
 
-    if (this.props.state_level_organization_name !== undefined){
-        const sloArray = Array.from(this.props.state_level_organization_name);
+    if (this.props.checkboxesDisplayed.state_level_organization_name !== null){
+        const sloArray = Array.from(this.props.checkboxesDisplayed.state_level_organization_name);
         stateLevelOrgCheckboxes = sloArray.map((setElement) => {
             return(
                 <div>
                     <Checkbox
                         value={setElement}
                         name="state_level_organization_name"
-                        defaultChecked
+                        checked={this.props.checkboxesSelected.state_level_organization_name.has(setElement)}
                         onChange={this.props.handleSloCheckbox}
                     />
                     <span>{setElement}</span>
                 </div>
             )
         })
+        if (stateLevelOrgCheckboxes.length > 0){
+            stateLevelOrgCheckboxes = [<div>
+                                <Checkbox
+                                    defaultChecked
+                                    onChange={this.props.handleSloCheckbox}
+                                    value="all"
+                                />
+                                <span>Select/Unselect All</span>
+                                </div>, 
+                                ...stateLevelOrgCheckboxes
+                            ]
+        }
     }
 
-    if (this.props.cohort_name !== undefined){
-        const cohortArray = Array.from(this.props.cohort_name);
+    if (this.props.checkboxesDisplayed.cohort_name !== null){
+        const cohortArray = Array.from(this.props.checkboxesDisplayed.cohort_name);
         cohortNameCheckboxes = cohortArray.map((setElement) => {
             return(
                 <div>
                     <Checkbox
                         value={setElement}
                         name="cohort_name"
-                        defaultChecked
+                        checked={this.props.checkboxesSelected.cohort_name.has(setElement)}                        
                         onChange={this.props.handleCohortCheckbox}
                     />
                     <span>{setElement}</span>
                 </div>
             )
         })
+        if (cohortNameCheckboxes.length > 0){
+            cohortNameCheckboxes = [<div>
+                                <Checkbox
+                                    defaultChecked
+                                    onChange={this.props.handleCohortCheckbox}
+                                    value="all"
+                                />
+                                <span>Select/Unselect All</span>
+                                </div>, 
+                                ...cohortNameCheckboxes
+                            ]
+        }
     }
 
     const statusCheckboxes = 
