8743ce45d700401b89cff43b967826b58c392a9a
Nathan Carroll <ntc238@gmail.com>
2018-10-26 13:29:26 -0500

    redirect fix from cohort details page also

--- src/components/CohortDetails/CohortDetails.js
+++ src/components/CohortDetails/CohortDetails.js
@@ -115,7 +115,7 @@ class CohortDetails extends Component {
 
   componentDidUpdate(prevProps) {
     if (!this.props.user.isLoading && this.props.user.userName === null) {
-      this.props.history.push("home");
+      this.props.history.push("/#");
     }
 
     if (
