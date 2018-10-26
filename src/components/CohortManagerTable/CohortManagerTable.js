a43af019db5584963e9f69ffb79db5e94a7318a2
Nathan Carroll <ntc238@gmail.com>
2018-10-26 12:34:09 -0500

    sort function triggers by clicking on columns

--- src/components/CohortManagerTable/CohortManagerTable.js
+++ src/components/CohortManagerTable/CohortManagerTable.js
@@ -52,13 +52,52 @@ export class CohortManagerTable extends Component {
   }
 
   sortBy = (column) => {
+    let sortFunction;
+
+    const compare = (a, b) => {
+      if (a && b && a > b){
+          return (this.state.orderBy.ascending ? 1 : -1)
+      } else if (a && b && a < b){
+          return (this.state.orderBy.ascending ? -1 : 1)
+      } else {
+          return 0;
+      }
+    }
+
+    switch(column){
+      case 'cohort':
+        sortFunction = (a, b) => {
+            let c, d;
+            try {
+                c = a.cohort.cohort_name.trim();
+                d = b.cohort.cohort_name.trim();
+            } catch(err) {
+                console.log('failed to sort', a, b);
+            }
+            return compare(c, d)
+        }
+        break;
+      
+      case 'cycle':
+        sortFunction = (a, b) => {
+          let c, d;
+          try {
+            c = a.cohort.cohort_cycle;
+            d = b.cohort.cohort_cycle;
+          } catch(err){
+            console.log('failed to sort', a, b);
+          }
+        }
+    }
+
+    let sortedTrainers = this.state.localTrainers.sort(sortFunction);
+
+    console.log('firing off with ', column);
     this.setState({
       orderBy: {
-        ...this.state.orderBy,
+        ascending: !this.state.orderBy.ascending,
         columnName: column
       }
-    }, (column) => {
-      console.log(column);
     })
   }
 
@@ -262,31 +301,31 @@ export class CohortManagerTable extends Component {
                   Cycle {this.state.orderBy.columnName === 'cycle' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('first_name')}>
-                  First Name {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  First Name {this.state.orderBy.columnName === 'first_name' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('last_name')}>
-                  Last Name {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  Last Name {this.state.orderBy.columnName === 'last_name' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_1')}>
-                  Initial Training {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  Initial Training {this.state.orderBy.columnName === 'req_1' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_2')}>
-                  TTT Terms {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  TTT Terms {this.state.orderBy.columnName === 'req_2' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_3')}>
-                  Observed Training {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  Observed Training {this.state.orderBy.columnName === 'req_3' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_4')}>
-                  Cert. Workshop {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  Cert. Workshop {this.state.orderBy.columnName === 'req_4' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_5')}>
-                  C&amp;C Training 1 {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  C&amp;C Training 1 {this.state.orderBy.columnName === 'req_5' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_6')}>
-                  C&amp;C Training 2 {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  C&amp;C Training 2 {this.state.orderBy.columnName === 'req_6' ? sortByIndicator : ' '}
                 </TableCell>
                 <TableCell className={this.props.classes.tableCell} onClick={() => this.sortBy('req_7')}>
-                  Recertification {this.state.orderBy.columnName === 'cohort' ? sortByIndicator : ' '}
+                  Recertification {this.state.orderBy.columnName === 'req_7' ? sortByIndicator : ' '}
                 </TableCell>
               </TableRow>
             </TableHead>
